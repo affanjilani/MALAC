7ac52eb1e2e55fb31a29840350dcb5a884c6d351
waelloop <alhashemiw@gmail.com>
2018-11-18 01:28:18 -0500

    Resolves Issue #5

---
+++ MALACBE/MALACBE/sharpnessBlur.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+def sharpnessBlur(grayImage,threshold):
+    #our flag representing our criteria
+    blurred = False
+
+    #compute the laplacian
+    fm = cv2.Laplacian(grayImage,cv2.CV_64F).var()
+
+    #compare the sharpness to a threshold
+    if fm > threshold:
+        blurred = True
+
+    #check if we meet our criteria
+    if blurred:
+        print "WRONG FORMAT"
+        return False
+    else:
+        print "ACCEPTED FORMAT"
+        return True
